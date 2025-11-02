caed21de444e8ddbd36fa41e5f237c4ab2702da1
adheliaissabel <h1101241001@student.untan.ac.id>
2025-11-02 12:46:24 +0700

    up

--- config/database.php
+++ config/database.php
@@ -5,6 +5,6 @@ return [
     'port' => getenv('MYSQLPORT') ?: '3306',
     'dbname' => getenv('MYSQLDATABASE') ?: 'mvc_db',
     'username' => getenv('MYSQLUSER') ?: 'root',
-    'password' => getenv('MYSQLPASSWORD') ?: '',
+    'password' => getenv('MYSQLPASSWORD') ?: 'mkjw4004',
     'charset' => 'utf8mb4'
 ];
