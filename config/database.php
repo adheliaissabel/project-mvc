8957c83873ccfab0ce6ae57bff3317a0a09a2f5d
adheliaissabel <h1101241001@student.untan.ac.id>
2025-11-02 12:54:48 +0700

    bismillah

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 // Railway menyediakan MySQL variables otomatis
 return [
-    'host' => getenv('MYSQLHOST') ?: 'localhost',
+    'host' => getenv('MYSQLHOST') ?: '127.0.0.1',
     'port' => getenv('MYSQLPORT') ?: '3306',
     'dbname' => getenv('MYSQLDATABASE') ?: 'mvc_db',
     'username' => getenv('MYSQLUSER') ?: 'root',
